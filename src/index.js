fa07c17e0f3522c20e9b7b0ee4699108d4c6520f
Ilya Dzyuin <86766397+ilua-dz@users.noreply.github.com>
2021-08-17 02:48:19 +0400

    feat: fix 2 "zero" result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array.length ? [...array].sort((a, b) => a - b)[0] : 0;
+  return (array && array.length) ? [...array].sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return array.length ? [...array].sort((a, b) => b - a)[0] : 0;
+  return (array && array.length) ? [...array].sort((a, b) => b - a)[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return array.length ? Math.round([...array].reduce((sum, i) => sum + i) / array.length * 100) / 100 : 0;
+  return (array && array.length) ? Math.round([...array].reduce((sum, i) => sum + i) / array.length * 100) / 100 : 0;
 }
